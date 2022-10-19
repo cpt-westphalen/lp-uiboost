8ebfa84b344f2443b3b9c677f887b8393e327e26
rbwestphalen <rbwestphalen@gmail.com>
2022-10-19 11:04:09 -0300

    CSS: New defaults + todo (incomplete): basic page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,56 @@
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<div className='h-[44px]'>status bar</div>
+			<header>
+				logo
+				<nav>=</nav>
+			</header>
+			<div
+				id='1'
+				className='flex flex-col items-center'>
+				<div id='1-1'>
+					<p>Exclusivos para alunos uiBoost 📏</p>
+					<h1>
+						Estude design até em outra <span>galáxia</span>
+					</h1>
+					<p>
+						Baixe o app do uiBoost e tenha acesso a todo o conteúdo
+						do curso uiBoost na palma da sua mão, mesmo estando
+						offline. Aprenda de qualquer lugar e a qualquer hora.
+					</p>
+					<div id='download-icons'>
+						<div>Download via App Store</div>
+						<div>Download via Google Play</div>
+					</div>
+				</div>
+				<div id='1-2'>Smartphone App Visualization</div>
+				<div id='divider-arrow'>arrow-down</div>
+				<div id='2'>
+					<div id='2-1'>
+						<h1>O Boost para a sua carreira</h1>
+						<p>
+							Ajudamos designers a se especializarem em UI design
+						</p>
+					</div>
+					<div id='2-2'>
+						<div id='2-2-1'>
+							<span>300+</span>
+							<p>Alunos com certificações</p>
+						</div>
+						<div id='2-2-2'>
+							<span>60+</span>
+							<p>Horas de conteúdo gravado</p>
+						</div>
+						<div id='2-2-3'>
+							<span>5+</span>
+							<p>Horas de mentoria por turma</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
